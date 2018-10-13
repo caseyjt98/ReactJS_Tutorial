c21f1d18d93aa54fdbff3741b46ae466fc67eb83
Casey Taylor <caseyjt98@gmail.com>
2018-10-13 16:21:25 -0700

    add basic component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import './App.css';
 
 class App extends Component {
   render() {
+
+
     return (
       <div className="App">
           <h1> Ferds Bog </h1>
+          <p> Woohooo it's working </p>
       </div>
     );
+
+
   }
 }
 
---
+++ src/Person/Person.js
@@ -0,0 +1,9 @@
+import React from 'react' 
+
+const person = () => {
+    return (
+        <p> hello i am a person </p> 
+    );
+}
+
+export default person; 
